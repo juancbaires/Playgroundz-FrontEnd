9750bd7ed536aebb9f2e7c1247eb8264ac500867
Ezana Bahta <ezanab2@gmail.com>
2018-10-23 16:12:56 -0400

    Registration forum

--- src/Components/Signup/SignupForm.js
+++ src/Components/Signup/SignupForm.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
 
 function SignupForm() {
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Login
-      </Typography>
       <Grid container spacing={24}>
         <Grid item xs={12} sm={6}>
           <TextField
@@ -35,20 +32,43 @@ function SignupForm() {
         <Grid item xs={12}>
           <TextField
             required
+            id="email"
+            name="email"
+            type="email"
+            label="e-mail"
+            fullWidth
+            autoComplete="email"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="userName"
+            name="userName"
+            label="Username"
+            fullWidth
+            autoComplete="uname"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="password"
+            name="password"
+            type="password"
+            label="Password"
+            fullWidth
+            autoComplete="fpassword"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
             id="address1"
             name="address1"
             label="Address line 1"
             fullWidth
             autoComplete="billing address-line1"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="addiress2"
-            name="addiress2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="billing address-line2"
           />
         </Grid>
         <Grid item xs={12} sm={6}>
