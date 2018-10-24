710f3cf2cd6b59c84b3672f133d7228ef46bc331
Juan Baires <juancbaires@gmail.com>
2018-10-24 12:04:01 -0400

    fetched the backend

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+    state = {
+        event: []
+    }
+    componentDidMount() {
+        fetch('https://playgroundz-heroku.herokuapp.com/')
+            .then(res => res.json())
+            .then(event => this.setState({ event }))
+    }
+
+
     render() {
         return (
             <div>
-                <h1> TEsting </h1>
+                <h1>Events</h1>
+
+                {this.state.event.map(evt => {
+                    return (
+                        <ul>
+                            <li key={evt.id}>{evt.sport}{" "}{evt.locationName}</li>
+                            <li key={evt.id}>{" "}{evt.address.street}{evt.address.city}{evt.address.state}{evt.address.zip}</li>
+                            <li key={evt.id}>{" "}{evt.eventDate}</li>
+                            <li key={evt.id}><img src={evt.locationImg}></img></li>
+                            <li key={evt.id}>{evt.age}</li>
+                            <li key={evt.id}>{evt.rsvps.find(element => {
+                                return (
+                                    <span>{element}</span>
+                                )
+                            })}</li>
+
+                        </ul>
+                    )
+                })}
+
             </div>
         );
     }
 }
-
 export default Home;
