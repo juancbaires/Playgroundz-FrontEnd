84325d6d5f3091cad64a84a5bfac991a63065b16
Juan Baires <juancbaires@gmail.com>
2018-10-25 20:54:55 -0400

    issue creating now

--- src/Components/App.js
+++ src/Components/App.js
@@ -53,8 +53,7 @@ class App extends Component {
   }
   // handle signup
 
-  handleSignUp = (e) => {
-    e.preventDefault()
+  handleSignUp = () => {
     axios.post(`${env}/users/signup`, {
       email: this.state.email,
       password: this.state.password
@@ -73,8 +72,7 @@ class App extends Component {
 
   // handle log in
 
-  handleLogIn = (e) => {
-    e.preventDefault()
+  handleLogIn = () => {
     axios.post(`${env}/users/login`, {
       email: this.state.email,
       password: this.state.password
@@ -94,15 +92,15 @@ class App extends Component {
   render() {
     return (
       <div className="appBackground">
-        <Header />
+        <Header isLoggedIn={this.state.isLoggedIn} handleLogIn={this.handleLogIn} handleLogOut={this.handleLogOut} handleSignUp={this.handleSignUp} />
         <main>
           <Switch>
             <Route path="/create-event" render={() => <CreateEvent event={this.state} />} />
             {/* Sign-up Page */}
-            <Route path="/signup" render={() => <Signup isLoggedIn={this.state.isLoggedIn} handleInput={this.handleInput} handleSignUp={this.handleSignUp} signUpError={this.state.signUpError} />} />
+            <Route path="/signup" render={(props) => <Signup {...props} isLoggedIn={this.state.isLoggedIn} handleInput={this.handleInput} handleSignUp={this.handleSignUp} signUpError={this.state.signUpError} />} />
             {/* Login Page */}
             <Route path="/logout" render={() => <Logout isLoggedIn={this.state.isLoggedIn} handleLogOut={this.state.handleLogOut} />} />
-            <Route path="/login" render={() => <Login isLoggedIn={this.state.isLoggedIn} handleInput={this.handleInput} handleLogIn={this.handleLogIn} loginError={this.state.loginError} />} />
+            <Route path="/login" render={(props) => <Login {...props} isLoggedIn={this.state.isLoggedIn} handleInput={this.handleInput} handleLogIn={this.handleLogIn} loginError={this.state.loginError} />} />
             {/* Home Page / main landingpage */}
             <Route path="/" render={() => <Home isLoggedIn={this.state.isLoggedIn} />} />
           </Switch>
